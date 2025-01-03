13903f040e7a4b4cfb12935b40dd3be28a3551f2
Michael <miguelmotaitz@gmail.com>
2025-01-03 08:53:57 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
-$('#telefone').mascara('(00) 00000-0000', {
+$(document).ready(function() {
+    $('#carousel-imagens').slick({
+        autoplay: true, 
+    });
+
+$('#telefone').mask('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
-})
+});
 
-$('#cpf').mascara('000.000.000-00', {
+$('#cpf').mask('000.000.000-00', {
     placeholder: '123.456.789-00'
-})
+});
 
-$('#cep').mascara('00000-000', {
+$('#cep').mask('00000-000', {
     placeholder: '012345-678'
-})
+});
 
 $('form').validate({
     rules: {
@@ -36,7 +41,7 @@ $('form').validate({
         alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
         form.reset();
     },
-    invalidHandler: function (form, validator) {
+    invalidHandler: function (evento, validator) {
         alert("Por favor, preencha os campos para prosseguir com a compra!");
     }
 })
