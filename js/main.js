792d527516be88a3143444095d67217e052e31dc
Michael <miguelmotaitz@gmail.com>
2025-01-03 08:49:35 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+$(document).ready(function() {
+    $('#carousel-imagens').slick({
+        autoplay: true, 
+    });
+
+
 $('#telefone').mascara('(00) 00000-0000', {
     placeholder: '(DDD) 12345-6789'
 })
